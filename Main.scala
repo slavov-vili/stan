608ef4ac8d5228355e9b8ddf76364ae08259a38c
vilskin <slavov.vili@gmail.com>
2018-10-07 15:25:50 +0200

    Added initial files to repo

---
+++ Main.scala
@@ -0,0 +1,156 @@
+//package test
+
+import trie.Trie
+import ngrammer._
+import java.io.File
+import scala.io.Source
+
+object Main {
+	
+  def main(args: Array[String]) {
+    val dirName = "wordLemPos"
+    println("constructing the trie")
+    // the trie of words
+    val trie = Trie.TrieNode()
+    println("creating n-grams")
+
+    // for each file in the directory
+    // add all words to the trie
+    // extract all Ngrams from the files
+    for(filepath <- new File(dirName).listFiles
+                                     .filter(_.getName
+                                              .endsWith(".txt"))) {
+      trie.readFile(filepath)
+      NgramMaker.readFile(filepath)
+    } //end for
+
+    // count all the words in the texts
+    val totalWC = NgramMaker.unigramCount
+        .map{case (_,count) => count}.reduce(_ + _)
+
+    // read Stdin and respond
+    println("Enter a sentence:")
+    read_std_in(trie, totalWC)
+  } //end main
+
+
+
+  // reads from Stdin and responds to querries
+  // (corrects misspelled words)
+  def read_std_in(myTrie: trie.Trie.TrieNode, totalWC: Int): Unit = {
+    // for each line in the input
+    for(ln <- Source.stdin.getLines) {
+      // split the input into a list of lowercased words
+      // and print the corrected sentence
+      println(correct_sentence(ln.split(" ").map(x => x.toLowerCase),
+                               myTrie, totalWC))
+    } //end for
+  } //end def
+
+
+
+  // corrects all mistakes in the input sentence and returns the correct one
+  def correct_sentence(sent: Array[String], myTrie: trie.Trie.TrieNode,
+                       totalWC: Int): String = {
+    // while there are mistakes in the sentence
+    while (sent.exists(x => !myTrie.contains(x))) {
+      // get the index of the first misspelled word in the sentence
+      val i = sent.indexWhere(!myTrie.contains(_))
+      // store the word itself
+      val word = sent(i)
+
+      // get all words, close to the misspelled word
+      // and their distance from that word
+      val withinX = get_within_x(myTrie, word)
+      //withinX.foreach(println)
+      //withinX.foreach{case (w,d) => println(s"$w, $d, ${NgramMaker.unigramCount(w)}")}
+      val wordScores = withinX.map(x => calc_word_score(sent, i, x, totalWC))
+      wordScores.foreach(println)
+      sent(i) = wordScores.maxBy(_._2)._1
+      //wordScores.filter(_ == wordScores.maxBy(_._2)).foreach(println)
+    }  //end while
+    
+    // return the sentence
+    sent.mkString(" ")
+  } //end def
+
+
+
+  // gets all words within edit distance X of the given word
+  def get_within_x(theTrie: trie.Trie.TrieNode, 
+                    word: String): List[(String, Int)] = {
+    // first search for words within edit distance 2
+    var n = 2
+    var within = theTrie.search(word, n)
+    // if there are no such words
+    while (within.isEmpty && n < (word.length / 3)) {
+      n += 1
+      within = theTrie.search(word, n)
+    } //end if
+
+    within.toList//.map{case (w,d) => (w,d.toDouble)}//.map{case (w,d) => (w, Math.pow(2.0, d) / (w.length + word.length))}
+  } //end def
+
+
+
+  // calculates the score of the word given the context
+  def calc_word_score(sent: Array[String], idx: Int, 
+                      candidate: (String, Int),
+                      totalWC: Int): (String, Double) = {
+    //println(candidate)
+    val dist = candidate._2
+    sent(idx) = candidate._1
+    //println(s"index: $idx")
+    var sum = 0.0
+    // the sum of the previously calculated n-grams
+    var prevSum = 1.0
+
+    // for unigrams, bigrams and trigrams
+    for (i <- 1 to 3) {
+      //println(s"ngram: $i")
+      // temporary sum for the counts of each n-gram
+      var ngramSum = 0.0
+
+      // for all possible contexts of the word
+      for (j <- idx-(i-1) to idx) {
+        //println(s"starting index: $j")
+
+        // take the context
+        val context = sent.slice(j, j+i)
+        //println(context.mkString(" "))
+        
+        if (context.length == i) {
+          // add the count to the sum
+          i match {
+            //case 1 => ngramSum += 0
+            case 1 => {
+                val wc = NgramMaker.unigramCount.getOrElse(context(0), 0)
+                ngramSum += Math.sqrt(wc)
+                //ngramSum += Math.pow((ngramSum * Math.sqrt(wc)), 2)
+            } //end case
+            case 2 => {
+                val wc = NgramMaker.bigramCount
+                         .getOrElse((context(0), context(1)), 0)
+                ngramSum += wc
+            } //end case
+            case 3 => {
+                val wc = NgramMaker.trigramCount
+                         .getOrElse((context(0), context(1), context(2)), 0)
+                ngramSum += wc
+            } //end case
+          } //end match
+        } //end if
+      } //end for
+
+       //adjust sum based on n-gram weight and add it to the total sum
+      //sum += Math.pow(ngramSum, (1 / dist))
+      prevSum = Math.pow((prevSum * ngramSum), i)
+      sum += Math.pow((prevSum * ngramSum), i)
+    } //end for
+
+    // make a pair of the word and its score
+    (sent(idx), (sum / dist))
+  } //end def
+
+
+} //end object
---
+++ NgramMaker.scala
@@ -0,0 +1,140 @@
+//package ngrammer
+
+import java.io.File
+import java.nio.charset.CodingErrorAction
+import scala.io.Codec
+import scala.io.Source
+import scala.collection.mutable.HashMap
+
+object NgramMaker {
+
+  val unigramCount = HashMap[String, Int]()
+  val bigramCount = HashMap[(String, String), Int]()
+  val trigramCount = HashMap[(String, String, String), Int]()
+
+  def main(args: Array[String]): Unit = {
+    val path = getClass.getResource("wordLemPos").getPath
+    readFiles(path)
+    val arr1 = Array("run", "runs", "turn", "turns")
+    val arr2 = Array(("he", "run"),
+                     ("he", "runs"),
+                     ("he", "turns"),
+                     ("i", "run"),
+                     ("i", "runs"),
+                     ("i", "turn"),
+                     ("i", "turns"),
+                     ("run", "away"),
+                     ("runs", "away"),
+                     ("turn", "away"),
+                     ("turns", "away"))
+    val arr3 = Array(("he", "run", "away"),
+                     ("he", "runs", "away"),
+                     ("he", "turns", "away"),
+                     ("i", "run", "away"),
+                     ("i", "turn", "away"))
+    arr1.foreach(x => println(s"$x: ${unigramCount.getOrElse(x, 0)}"))
+    arr2.foreach(x => println(s"$x: ${bigramCount.getOrElse(x, 0)}"))
+    arr3.foreach(x => println(s"$x: ${trigramCount.getOrElse(x, 0)}"))
+    //unigramCount.foreach(println)
+    //bigramCount.foreach(println)
+    //trigramCount.foreach(println)
+    //println(unigramCount)
+    //println(trigramCount)
+  }   //end main
+
+
+
+  def readFiles(filepath: String): Unit = {
+    // get all the files from the filepath
+    val dir = new File(filepath)
+        //println("counting word frequency")
+        //println("counting bigram frequency")
+        //println("counting trigram frequency")
+    val files = dir.listFiles.filter(_.getName.endsWith(".txt")).toList
+    // read each file
+    files.foreach(readFile)
+  } //end def
+
+
+
+  def readFile(file: File): Unit = {
+    //println(s"reading: ${file.getName()}")
+    // detect and replace illegal UTF-8 byte sequences
+    // (needed for some of our input files)
+    implicit val codec = Codec("UTF-8")
+    codec.onMalformedInput(CodingErrorAction.REPLACE)
+    codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
+    
+    // make an iterator over all the words in each line
+    val wordIter = Source.fromFile(file)
+                         .getLines
+                         .map(x => takeWord(x))
+    var lastWord = ""
+    var sent = List[String]()
+    // for each word
+    for(word <- wordIter) {
+      // if at the end of a sentence
+      if(isEOS(lastWord) && word.matches("\\b[A-Z].*")) {
+        // get the separate sentences without punctuation
+        val cleanSent = sent.filter(_.matches("[A-Za-z0-9].*"))
+                           .reverse
+                           .map(_.toLowerCase)
+        cleanSent.foreach(x => addToMap(x, unigramCount))
+        //println(cleanSent.mkString(" "))
+        
+        // extract all bigrams from the sentence
+        val newBigrams = cleanSent.sliding(2).toList
+          .collect{case List(x,y) => (x,y)}
+          //newBigrams.foreach(println)
+        newBigrams.foreach(addToMap(_, bigramCount))
+        //println(newBigrams)
+        
+        // extract all the trigrams from the sentence
+        val newTrigrams = cleanSent.sliding(3).toList
+          .collect{case List(x,y,z) => (x,y,z)}
+        newTrigrams.foreach(addToMap(_, trigramCount))
+        //println(newTrigrams)
+
+        sent = List(word)
+      } //end if
+      else
+        sent = word :: sent
+
+      lastWord = word
+    } //end for
+  } //end def
+
+
+
+  // adds a key to a hashmap
+  def addToMap[T](key: T, map: HashMap[T, Int]): Unit = {
+  if(map.contains(key))
+     map(key) += 1
+   else
+     map(key) = 1
+  } //end def
+
+
+
+  // checks if a string represents an EndOfSentence symbol
+  def isEOS(str: String): Boolean = {
+    val res = if(str == "." || str == "!" || str == "?")
+                true
+              else
+                false
+    res
+  } //end def
+
+
+
+  // extracts the word from line
+  def takeWord(line: String): String = {
+    val parts = line.split("\t")
+    val res = if (parts.isDefinedAt(0))
+                parts(0)
+              else
+                ""
+    res
+  } //end def
+
+} //end object
